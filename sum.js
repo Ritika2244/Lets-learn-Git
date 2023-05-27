1da0f891987afa2d990f2c1a78798700d30a42b6
Ritika2244 <ritikakaushik2244@gmail.com>
2023-05-27 14:38:39 +0530

    innitial commit

---
+++ sum.js
@@ -0,0 +1,33 @@
+let cap={
+   name:"Steve",
+   lastName:"Morgan",
+   address:{
+      city:"Meerut",
+      state:"new york",
+   }
+   ,isAvenger:true,
+   movies: ["FirstAvenger","SecondAvenger"],
+   sayHi: function(){
+      console.log("Cap say");
+   }
+};
+
+//GET
+/*
+console.log(cap.age);
+console.log(cap.movies[1]);
+cap.sayHi(); */
+
+/*console.log("cap:0,cap");
+cap.isAvenger=false;
+cap.friends=["Tony","keyuie "]
+cap.age=36;
+*/
+
+
+
+
+
+
+
+
